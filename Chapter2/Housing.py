9a76eb1bf9d7a7e5e5d46f4aeca797c51b30935c
George Ashley <george.badash@gmail.com>
2017-12-30 21:42:12 -0800

    Initial commit

---
+++ Chapter2/Housing.py
@@ -0,0 +1,151 @@
+import os
+import tarfile
+import urllib
+from six.moves import urllib
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import Imputer, OneHotEncoder, StandardScaler
+from AddClasses import CombinedAttributesAdder, DataFrameSelector, CategoricalEncoder
+
+#Download data (California housing)
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL,housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path= HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+#Identical Function
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data)*test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+fetch_housing_data()
+housing = load_housing_data()
+#train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42) #Same as defined function
+#print(housing.describe())
+#housing.hist(bins=50, figsize=(20, 15))
+#plt.show()
+
+#Begin Create testing set
+housing["income_cat"] = np.ceil(housing["median_income"]/1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+#Split test data and training data for validation
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+#print(strat_test_set["income_cat"].value_counts()/len(strat_test_set))
+
+#Remove income_cat attribute from data
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+#End Create testing set
+
+#Map location and price
+housing = strat_train_set.copy()
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1,
+             s=housing["population"]/100, label="population", figsize=(10,7),
+             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,)
+plt.legend()
+#plt.show()
+
+#Calculate Standard correlation coefficient
+#corr_matrix = housing.corr()
+#print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+#Add custom attribute combinations
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_home"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"] = housing["population"]/housing["households"]
+corr_matrix = housing.corr()
+#print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+#Revert to clean training set
+housing = strat_train_set.drop("median_house_value",axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+#Begin data cleaning - 3 Options for missing data (numerical)
+# housing.dropna(subset=["total_bedrooms"])
+# housing.drop("total_bedrooms", axis=1)
+# median = housing["total_bedrooms"].median()
+# housing["total_bedrooms"].fillna(median, inplace=True)
+
+#OR sklean method
+imputer = Imputer(strategy="median")
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer.fit(housing_num)
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+#Convert the ocean distances to numbers
+housing_cat = housing["ocean_proximity"]
+housing_cat_encoded, housing_categories = housing_cat.factorize()
+#print(housing_cat_encoded[:10])
+
+#Use one-hot encoding to make these categories which are 0s and 1
+encoder = OneHotEncoder() # At some point CategoricalEncoder will be released to do these two steps simultaneously
+housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1, 1))
+
+#Using custom transformer to add hyperparameter. Can be easily removed or added to tweak ML performance
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+#Feature scaling - included below
+
+#Do all the steps above with pipelines
+num_pipeline = Pipeline([
+    ('imputer', Imputer(strategy="median")),
+    ('attribs_addr', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+])
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+#Using classes to refactor pipelines
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+num_pipeline = Pipeline([
+        ('selector', DataFrameSelector(num_attribs)),
+        ('imputer', Imputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+
+cat_pipeline = Pipeline([
+        ('selector', DataFrameSelector(cat_attribs)),
+        ('cat_encoder', CategoricalEncoder(encoding="onehot-dense")),
+    ])
+
+full_pipeline = FeatureUnion(transformer_list=[
+        ("num_pipeline", num_pipeline),
+        ("cat_pipeline", cat_pipeline),
+    ])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+print(housing_prepared.shape)  # (16512, 16)
+
+#Selecting and training a model
